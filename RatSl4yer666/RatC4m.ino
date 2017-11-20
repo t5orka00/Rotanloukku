c23df5cb8d1c8b82f53db3733c57c99d77845e55
t6jaje00 <33248933+t6jaje00@users.noreply.github.com>
2017-11-20 14:10:48 +0200

    Add files via upload

---
+++ RatSl4yer666/RatC4m.ino
@@ -0,0 +1,37 @@
+#include <Adafruit_VC0706.h>// Kamera
+
+SoftwareSerial cameraconnection = SoftwareSerial(3, 4);   // RX ja TX kameralle
+
+Adafruit_VC0706 cam = Adafruit_VC0706(&cameraconnection);
+
+void Camera() 
+{
+  cam.begin();    // Käynnistetään kamera
+  delay(500);
+  // Haluttu kuvakoko
+  //cam.setImageSize(VC0706_640x480);        // biggest
+  //cam.setImageSize(VC0706_320x240);        // medium
+  cam.setImageSize(VC0706_160x120);          // small
+
+  // Pieni viive jotta käynnistys on tapahtunut
+  delay(1000);
+
+  cam.takePicture();  // Otetaan Kuva
+
+    do {
+    delay(10); 
+  } while (Serial.available() && Serial.read() >= 0);
+
+  // Get the size of the image (frame) taken  
+  uint16_t jpglen = cam.frameLength();
+  pinMode(8, OUTPUT);
+  // Read all the data up to # bytes!
+
+  while (jpglen > 0) {
+    uint8_t *buffer;
+    uint8_t bytesToRead = min(64, jpglen); // jos 64 jumittaa niin nopeudeks 32
+    buffer = cam.readPicture(bytesToRead);
+    xbee.write(buffer,bytesToRead);
+    jpglen -= bytesToRead;
+  }
+}
---
+++ RatSl4yer666/RatSl4yer666.ino
@@ -0,0 +1,108 @@
+/*
+ * Board: Arduino Pro tai Arduino Pro Mini
+ * Processor: ATmega328P 8MHz 3.3V - toiminee myös muutki mutta käytetään tätä
+ */
+
+#include <avr/sleep.h>      // lepotila, interrupt
+
+#include <SoftwareSerial.h> // XBee ja kamera käyttää 
+
+SoftwareSerial xbee(5, 6);  // RX, TX.  XBee serial pinnit
+
+const int wakePin = 2;      // Interrupt pinni 
+
+int wakeVal = 0;
+int wakeState = LOW;
+
+void wakeUpNow() {          // Interrupt
+  sleep_disable();          // Poistutaan lepotilasta
+  detachInterrupt(0);       // Interrupt väliaikaisesti pois käytöstä
+} 
+
+void setup() {
+  Serial.begin(9600);
+  //pinMode(10, OUTPUT);
+
+  pinMode(5, INPUT);        // XBee:n RX ja TX, varmuuden vuoksi erikseen määritellään täällä
+  pinMode(6, OUTPUT); 
+
+  // Interrupt
+  pinMode(wakePin, INPUT);  
+  //attachInterrupt(0, sleepNow, LOW); // use interrupt 0 (pin 2) and run function wakeUpNow when pin 2 gets LOW  
+
+  // XBee
+  xbee.begin( 115200 );
+}  
+
+void sleepNow() {  
+    //pinMode(wakePin, INPUT);  
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode is set here  
+    cli();
+    sleep_enable();          // enables the sleep bit in the mcucr register  
+    attachInterrupt(0,wakeUpNow, RISING); // use interrupt 0 (pin 2) and run function  
+    sei();
+    //sleep_mode();            // here the device is actually put to sleep!!  
+    sleep_cpu (); 
+} 
+/*
+void disSleep(){
+  // THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP  
+    sleep_disable();         // first thing after waking from sleep: disable sleep...  
+    detachInterrupt(0);      // disables interrupt 0 on pin 2 so the wakeUpNow code will not be executed during normal running time.  
+}*/
+
+void loop() {
+  //disSleep();
+  //Serial.println("KAMERA OTTAA KUVAN");
+  //delay(10);
+  //wakeVal = digitalRead(wakePin);
+  //while(wakeVal == 1)
+  //cli();
+  byte triesBeforeSleep = 10; byte i = 0;
+  Serial.println("Otetaan kuva");
+  xbee.write( "aloita" );
+  delay(500);
+  Camera();
+  delay(500);
+  xbee.write( "lopeta" );
+  delay(4000);
+  
+  Serial.println("Kuva otettu");
+  while(digitalRead(wakePin)) //|| ++i < triesBeforeSleep)
+  {
+    //wakeVal = digitalRead(wakePin);
+    //disSleep();
+    Serial.println("while looppi");
+    delay(500);
+    
+    
+    //delay(10);
+  }
+  Serial.println("Menossa nukkumaan");
+  //delay(500);
+  //sei();
+  
+  sleepNow();     // sleep function called here
+  delay(5000);  
+  
+  
+  /*
+  wakeVal = digitalRead(wakePin);
+  if (wakeVal == HIGH)
+  {  disSleep();
+     if (wakeState == LOW)
+     {
+      Serial.println("Motion detected");
+      wakeState = HIGH;
+     }
+  }
+  else{
+    
+    if (wakeState == HIGH){
+      delay(100);
+      Serial.println("Motion stopped - Going to Sleep Mode !");
+      wakeState = LOW;
+      sleepNow();     // sleep function called here
+    }
+  */  
+}
