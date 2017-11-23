b76e1e03e1050d2e068bb5b198de758b18ad3ed7
t6jaje00 <33248933+t6jaje00@users.noreply.github.com>
2017-11-23 10:17:34 +0200

    Add files via upload

---
+++ RatSl4yer666_23/RatC4m.ino
@@ -0,0 +1,37 @@
+#include <Adafruit_VC0706.h>// Kamera
+
+SoftwareSerial cameraconnection = SoftwareSerial(3, 4);   // RX ja TX kameralle
+
+Adafruit_VC0706 cam = Adafruit_VC0706(&cameraconnection);
+
+void Camera() 
+{
+  cam.begin();    // Käynnistetään kamera
+  // Pieni viive jotta käynnistys on tapahtunut 
+  delay(350); // tässä pitää olla delay, muuten ei tuu kuvat, ei kerkeä käynnistyä, 500 toimii 200 ei toimi, 300 tuntuu pelaavan aina, testattu pienimmällä resolla
+  
+  // Haluttu kuvakoko
+  //cam.setImageSize(VC0706_640x480);        // biggest
+  //cam.setImageSize(VC0706_320x240);        // medium
+  cam.setImageSize(VC0706_160x120);          // small
+  delay(50); // reson vaihtoon, voi olla ettei vaihda ilman delaytä
+
+  cam.takePicture();  // Otetaan Kuva
+
+    do {
+    delay(10); 
+  } while (Serial.available() && Serial.read() >= 0);
+  delay(50);
+  // Get the size of the image (frame) taken  
+  uint16_t jpglen = cam.frameLength();
+  pinMode(8, OUTPUT);
+  // Read all the data up to # bytes!
+
+  while (jpglen > 0) {
+    uint8_t *buffer;
+    uint8_t bytesToRead = min(64, jpglen); // jos 64 jumittaa niin nopeudeks 32
+    buffer = cam.readPicture(bytesToRead);
+    xbee.write(buffer,bytesToRead);
+    jpglen -= bytesToRead;
+  }
+}
---
+++ RatSl4yer666_23/RatSl4yer666_23.ino
@@ -0,0 +1,87 @@
+/*
+ * Board: Arduino Pro tai Arduino Pro Mini
+ * Processor: ATmega328P 8MHz 3.3V - toiminee myös muutki mutta käytetään tätä
+ */
+#include <avr/interrupt.h>
+#include <avr/sleep.h>      // lepotila, interrupt
+#include <avr/power.h>
+
+#include <SoftwareSerial.h> // XBee ja kamera käyttää 
+
+SoftwareSerial xbee(5, 6);  // RX, TX.  XBee serial pinnit
+
+const int wakePin = 2;      // Interrupt pinni 
+
+int wakeVal = 0;
+int wakeState = LOW;
+
+void wakeUpNow() {          // Interrupt
+  sleep_disable();          // Poistutaan lepotilasta
+  detachInterrupt(0);       // Interrupt väliaikaisesti pois käytöstä
+  
+} 
+
+void setup() {
+  Serial.begin(9600);
+  //pinMode(10, OUTPUT);
+
+  pinMode(5, INPUT);        // XBee:n RX ja TX, varmuuden vuoksi erikseen määritellään täällä
+  pinMode(6, OUTPUT); 
+
+  // Interrupt
+  pinMode(wakePin, INPUT);  
+  PCIFR = bit (PCIF1);
+  //ADCSRA = 0x00;  // ADC pois
+
+  //GIMSK &= ~_BV(INT0); // disable hardware INT0
+  //MCUCR &= ~(_BV(ISC01) | _BV(ISC00)); // set hardware INT0 active on low LEVEL
+
+  // XBee
+  xbee.begin( 115200 );
+}  
+
+void sleepNow() {    
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode is set here 
+    noInterrupts();
+    sleep_enable();          // enables the sleep bit in the mcucr register 
+    attachInterrupt(0, wakeUpNow, RISING); //attachInterrupt(0, wakeUpNow, RISING); // use interrupt 0 (pin 2) and run function
+    EIFR = bit (INTF0); // ilman tätä menee interruptit pieleen, herää samantein ku menee nukkuun // clear any outstanding interrupt 0
+
+    // Brown-Out Detection pois
+    MCUCR = bit (BODS) | bit (BODSE);
+    // The BODS bit is automatically cleared after three clock cycles
+    MCUCR = bit (BODS); 
+    
+    interrupts();
+    sleep_cpu();      
+} 
+
+void loop() {
+
+  byte triesBeforeSleep = 5; byte i = 0;
+  Serial.println("Otetaan kuva");
+  xbee.write( "aloita" );
+  //delay(500);
+  delay(10);
+  Camera();
+  //delay(500);
+  delay(10);
+  xbee.write( "lopeta" );
+  Serial.println("Kuva otettu"); 
+  
+  while((digitalRead(wakePin)) || (++i < triesBeforeSleep))
+  {
+    if (digitalRead(wakePin)) { i = 0;}
+    //wakeVal = digitalRead(wakePin);
+    //disSleep();
+    //Serial.print("a");
+    //Serial.print("while looppi ");
+    Serial.println(digitalRead(wakePin));
+    delay(500);
+  }
+  delay(500);
+  Serial.println(digitalRead(wakePin));Serial.println("Nukkuun"); 
+  delay(20);
+  Serial.flush();
+  sleepNow();     // sleep function called here 
+}
